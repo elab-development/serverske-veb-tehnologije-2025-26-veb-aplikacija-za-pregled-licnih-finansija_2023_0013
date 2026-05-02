444c81101cab0de5fc8ba24beab01dbfc536cb0d
Vuk Laketić <vuklaketic@gmail.com>
2026-05-03 00:58:07 +0200

    ispravio observer - mora created umesto creating

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -11,7 +11,7 @@ class UserObserver
     {
     }
 
-    public function creating(User $user): void
+    public function created(User $user): void
     {
         $this->seedDefaults->handle($user);
     }
