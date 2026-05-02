c50df6e2f56abd95048df64b117e21b952cc55a8
Vuk Laketić <vuklaketic@gmail.com>
2026-05-03 00:55:23 +0200

    CategoryFactory

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Category>
+ */
+class CategoryFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'name' => $this->faker->word(),
+            'type' => $this->faker->randomElement([Category::TYPE_INCOME, Category::TYPE_EXPENSE]),
+            'color' => $this->faker->hexColor(),
+            'icon' => 'bi-tag',
+        ];
+    }
+
+    public function income(): static
+    {
+        return $this->state(fn () => ['type' => Category::TYPE_INCOME]);
+    }
+
+    public function expense(): static
+    {
+        return $this->state(fn () => ['type' => Category::TYPE_EXPENSE]);
+    }
+}
