55de79a0c4c47e4827197d822c0d6406500f398f
Vuk Laketić <vuklaketic@gmail.com>
2026-05-03 00:56:34 +0200

    UserObserver seeduje kategorije pri kreiranju

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Observers\UserObserver;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -13,6 +15,7 @@ use Illuminate\Notifications\Notifiable;
 
 #[Fillable(['name', 'email', 'password', 'role', 'is_active', 'email_notifications'])]
 #[Hidden(['password', 'remember_token'])]
+#[ObservedBy([UserObserver::class])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Observers;
+
+use App\Actions\SeedDefaultCategories;
+use App\Models\User;
+
+class UserObserver
+{
+    public function __construct(private readonly SeedDefaultCategories $seedDefaults)
+    {
+    }
+
+    public function creating(User $user): void
+    {
+        $this->seedDefaults->handle($user);
+    }
+}
