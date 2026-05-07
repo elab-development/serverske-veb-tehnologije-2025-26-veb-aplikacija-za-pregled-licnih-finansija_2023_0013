f929c5f52f750c313c3b5ff9a21e776e7c931792
Vuk Laketić <vuklaketic@gmail.com>
2026-05-07 12:45:55 +0200

    agregat ukupnog bilansa

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -9,6 +10,18 @@ class DashboardController extends Controller
 {
     public function index(Request $request): View
     {
-        return view('dashboard');
+        $user = $request->user();
+
+        $totalIncome = (float) $user->transactions()
+            ->where('type', Transaction::TYPE_INCOME)
+            ->sum('amount');
+
+        $totalExpense = (float) $user->transactions()
+            ->where('type', Transaction::TYPE_EXPENSE)
+            ->sum('amount');
+
+        $balance = $totalIncome - $totalExpense;
+
+        return view('dashboard', compact('balance'));
     }
 }
