510817f6cb6292ca2d0659d324195caaf7e977df
Vuk Laketić <vuklaketic@gmail.com>
2026-05-07 12:46:56 +0200

    dodao Chart.js preko CDN

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.6/dist/chart.umd.min.js" defer></script>
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
